c6cff0b2da759bf2e7eb275de9fe1f72ddbf44f0
mohamedanies <modysalem22@yahoo.com>
2020-03-22 02:07:30 +0200

    navbar and logout

--- CrowdFunding/Users/urls.py
+++ CrowdFunding/Users/urls.py
@@ -7,6 +7,7 @@ app_name = 'Users'
 urlpatterns=[
     path('register/',views.users_register,name='register'),
     path('login/',views.users_login,name='login'),
+    path('logout/', views.users_logout, name='logout'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     # path('profile/',views.user_profile,name='profile'),
