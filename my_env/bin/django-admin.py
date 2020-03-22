826133783038ae01a7531b52922117a971b791aa
Fatma Akram <fatma.khater96@gmail.com>
2020-03-23 00:42:12 +0200

    fixed untracked files

--- my_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/alaa/python/django_crowd_funding_iti_40_project/my_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
